bfa5a2fe6673d48b7d1ef2a4ffe7ed26e26d099b
Xaron11 <kaczmarzykdawid4@gmail.com>
2025-03-20 17:36:52 +0100

    feat: add initial project setup with CMake, README, .gitignore and example main.cpp

---
+++ README.md
@@ -0,0 +1,25 @@
+# Download
+```sh
+git clone ...
+```
+
+# Build
+```sh
+cd ...
+mkdir build
+cd build
+cmake ..
+```
+```sh
+ninja
+```
+or
+```sh
+make
+```
+
+# Run
+Inside `build/`
+```sh
+./EAN
+````
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/Interval.h"
+#include <iostream>
+#include <string>
+
+int main() {
+  interval_arithmetic::Interval<double> x(1, 2);
+  interval_arithmetic::Interval<double> y(3, 4);
+  interval_arithmetic::Interval<double> z = x + y;
+  std::string left, right;
+  z.IEndsToStrings(left, right);
+  std::cout << "[" << left << ";" << right << "]" << std::endl;
+  return 0;
+}
